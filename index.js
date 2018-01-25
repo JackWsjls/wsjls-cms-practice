dd550236305ac7c0fd204116f9ac56753e17aee0
JackWsjls <418383219@qq.com>
2018-01-25 23:03:35 +0800

    'updated'

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ app.set('view engine', 'ejs');
 // });
 
 app.get("/",function(req,res){
-    res.render("login", {});
+    res.redirect("/login");
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
 });
 
 app.get("/wsjls_echarts",function(req,res){
